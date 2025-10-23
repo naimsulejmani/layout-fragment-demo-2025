a57ec7f11761b2bc1195da11e6e6798fa6a98adc
NaimSulejmani <naim.sulejmani@gmail.com>
2025-10-23 19:22:26 +0200

    Layout added

---
+++ src/main/java/dev/naimsulejmani/layoutfragmentdemo/HomeController.java
@@ -0,0 +1,23 @@
+package dev.naimsulejmani.layoutfragmentdemo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/gallery")
+    public String gallery() {
+        return "gallery";
+    }
+
+    @GetMapping("/contact")
+    public String contact() {
+        return "contact";
+    }
+}
---
+++ src/main/java/dev/naimsulejmani/layoutfragmentdemo/LayoutFragmentDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.naimsulejmani.layoutfragmentdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LayoutFragmentDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LayoutFragmentDemoApplication.class, args);
+    }
+
+}
