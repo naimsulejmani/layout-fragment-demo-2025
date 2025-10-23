a7ef8eec5d5d9aa835523d8ce373b4b3539fe68c
NaimSulejmani <naim.sulejmani@gmail.com>
2025-10-23 19:54:24 +0200

    list, object, classes, etc

--- src/main/java/dev/naimsulejmani/layoutfragmentdemo/HomeController.java
+++ src/main/java/dev/naimsulejmani/layoutfragmentdemo/HomeController.java
@@ -1,7 +1,10 @@
 package dev.naimsulejmani.layoutfragmentdemo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -19,5 +22,18 @@ public class HomeController {
     @GetMapping("/contact")
     public String contact() {
         return "contact";
+    }
+
+    @GetMapping("/news")
+    public String news(Model model) {
+        var news = List.of(
+                new News("Barca shume e forte!",
+                        "Shume te fortje jane, jo veq per Olimpiakos por edhe per shume tjere"
+                ),
+                new News("Reali me zorr fiton!",
+                        "Bajn cka tbojn disi fitojne, ja me hile ja me hile!")
+        );
+        model.addAttribute("news", news);
+        return "news";
     }
 }
---
+++ src/main/java/dev/naimsulejmani/layoutfragmentdemo/News.java
@@ -0,0 +1,13 @@
+package dev.naimsulejmani.layoutfragmentdemo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class News {
+    private String title;
+    private String content;
+}
